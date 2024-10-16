b35f28dcecc406e3796b944b20d52859fa47507d
fjosuem <josue@montes.cc>
2024-10-15 22:08:55 -0600

    feat: refactoring de pantallas

---
+++ IDC/DTOs/DetalleItemDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace IDC.DTOs
+{
+    public class DetalleItemDTO
+    {
+        public string? Titulo { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Nombre { get; set; }
+        public string? UrlImagen { get; set; }
+        public string? UrlVideoYoutube { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public DateTime? FechaEdicion { get; set; }
+    }
+}
--- IDC/DTOs/ItemDTO.cs
+++ IDC/DTOs/ItemDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace IDC.DTOs
+﻿using IDC.Pages;
+
+namespace IDC.DTOs
 {
     public class ItemDTO
     {
@@ -14,9 +16,7 @@
 
     public class QuienesSomosDTO : ItemDTO
     {
-        public int IdQuienesSomos { get; set; }  // Campo específico de QuienesSomos
-
-        // Sobreescribir la propiedad Id para mapearla a IdQuienesSomos
+        public int IdQuienesSomos { get; set; }
         public override int Id
         {
             get => IdQuienesSomos;
@@ -25,17 +25,80 @@
         public string? UrlImagenQuienesSomos { get; set; }
     }
 
+    public class DoctrinaDTO : ItemDTO
+    {
+        public int IdDoctrina { get; set; }
+        public override int Id
+        {
+            get => IdDoctrina;
+            set => IdDoctrina = value;
+        }
+        public string? UrlImagenDoctrina { get; set; }
+    }
+    public class DevocionalDTO : ItemDTO
+    {
+        public int IdDevocional { get; set; }
+        public override int Id
+        {
+            get => IdDevocional;
+            set => IdDevocional = value;
+        }
+        public string? UrlImagenDevocional { get; set; }
+    }
+
+    public class CortoDTO : ItemDTO
+    {
+        public int IdCorto { get; set; }
+        public override int Id
+        {
+            get => IdCorto;
+            set => IdCorto = value;
+        }
+        public string? UrlImagenCorto { get; set; }
+    }
+
+    public class LeninDTO : ItemDTO
+    {
+        public int IdLenin { get; set; }
+        public override int Id
+        {
+            get => IdLenin;
+            set => IdLenin = value;
+        }
+        public string? UrlImagenLenin { get; set; }
+    }
+
+    public class GriegoDTO : ItemDTO
+    {
+        public int IdGriego { get; set; }
+        public override int Id
+        {
+            get => IdGriego;
+            set => IdGriego = value;
+        }
+        public string? UrlImagenGriego { get; set; }
+    }
+
     public class AlabanzaDTO : ItemDTO
     {
-        public int IdAlabanza { get; set; }  // Campo específico de QuienesSomos
-
-        // Sobreescribir la propiedad Id para mapearla a IdQuienesSomos
+        public int IdAlabanza { get; set; }
         public override int Id
         {
             get => IdAlabanza;
             set => IdAlabanza = value;
         }
-        public string? UrlImagenAabanza { get; set; }
+        public string? UrlImagenAlabanza { get; set; }
+    }
+    
+    public class DirectorioDTO : ItemDTO
+    {
+        public int IdDirectorio { get; set; }
+        public override int Id
+        {
+            get => IdDirectorio;
+            set => IdDirectorio = value;
+        }
+        public string? UrlImagenDirectorio { get; set; }
     }
 
 }
